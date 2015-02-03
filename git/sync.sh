445deefd01a278196123a18ec2b47fb6000606c6
Parker DeBardelaben <pdebardelaben@gmail.com>
2015-02-02 20:27:31 -0500

    cleanup git sync

--- git/sync.sh
+++ git/sync.sh
@@ -2,4 +2,8 @@
 
 branch=$(git rev-parse --abbrev-ref head)
 
-git co master && git pull upstream master && git push origin master && git co $branch
+if [[ "$branch" == "master" ]]; then
+  git pull upstream master && git push origin master
+else
+  git co master && git pull upstream master && git push origin master && git co $branch
+fi
