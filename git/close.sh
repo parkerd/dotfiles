9561f6dbec1f3cb2645ab6cae98ce02131e51555
Parker DeBardelaben <pdebardelaben@gmail.com>
2015-02-03 20:17:20 -0500

    add git open/close

---
+++ git/close.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+branch=$(git rev-parse --abbrev-ref head)
+
+if [[ "$branch" == "master" ]]; then
+  if [ -z $1 ]; then
+    echo "usage: $0 [branch]"
+  else
+    branch=$1
+    git branch -d $branch && git push origin :$branch
+  fi
+else
+  git co master && git sync && git branch -d $branch && git remote prune origin
+fi
