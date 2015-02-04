d5e011023e296abb6be013045fcf49de0e93942c
Parker DeBardelaben <pdebardelaben@gmail.com>
2015-02-03 20:42:48 -0500

    add new git alias

--- git/close.sh
+++ git/close.sh
@@ -5,6 +5,7 @@ branch=$(git rev-parse --abbrev-ref head)
 if [[ "$branch" == "master" ]]; then
   if [ -z $1 ]; then
     echo "usage: $0 [branch]"
+    exit 1
   else
     branch=$1
     git branch -d $branch && git push origin :$branch
