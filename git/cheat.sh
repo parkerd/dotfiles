750ff0131aa4806bc63d1507791b2827db6ff488
Parker DeBardelaben <pdebardelaben@gmail.com>
2015-03-14 11:04:29 -0400

    auto pop changes per branch

--- git/cheat.sh
+++ git/cheat.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-echo -e "git open\t\tstart a new branch"
-echo -e "git cm \"msg\"\t\tcommit all changes"
+echo -e "git br\t\t\tlist branches"
+echo -e "git co <br>\t\tcheckout branch, stash/pop branch changes"
+echo -e "git cm \"msg\"\t\tcommit all changed files"
+echo -e "git st\t\t\tgit status"
+echo
+echo -e "git open <br>\t\tstart a new branch"
+echo -e "git close [br]\t\tsync merged pull request and delete branch"
 echo -e "git pr\t\t\tupdate master, rebase, open pull request"
 echo -e "git prr\t\t\topen pull request list in browser"
-echo -e "git close [br]\t\tsync merged pull request and delete branch"
 echo -e "git cs\t\t\tci-status for current branch pull request"
 echo -e "git css\t\t\topen jenkins log in browser"
 echo
-echo -e "git br\t\t\tlist branches"
 echo -e "git df\t\t\tshow current diff"
 echo -e "git dfl\t\t\tshow diff for last commit"
 echo -e "git dfr <cmt> [cmt]\tshow diff for single commit or between commits"
 echo -e "git lg\t\t\tshow last 10 commits"
 echo -e "git lgg\t\t\tshow full commit history"
-echo -e "git st\t\t\tgit status"
 echo
 echo -e "git in [rmt] [br]\tshow incoming commits for a remote/branch"
 echo -e "git out [rmt]\t\tshow outgoing commits for a remote"
--- git/checkout.sh
+++ git/checkout.sh
@@ -3,5 +3,14 @@
 if [ -z "$1" ]; then
   echo "usage: $0 <branch>"
 else
-  git stash -u >/dev/null && git checkout $@ && git stash list  
+  git stash save --quiet --include-untracked "$(date)"
+  git checkout $@
+  branch=$(git rev-parse --abbrev-ref head)
+  list=$(git stash list | grep "On ${branch}:")
+  if [ $(git stash list | grep -c "On ${branch}:") -eq 1 ]; then
+    git stash pop --quiet $(echo $list | cut -d: -f1)
+    git st
+  else
+    echo -n $list
+  fi
 fi
