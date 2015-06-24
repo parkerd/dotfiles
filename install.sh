9fe0cfa7a91f42df9cf64aa6011b0d295aa5978b
Parker DeBardelaben <pdebardelaben@gmail.com>
2015-06-24 11:20:04 -0400

    fix install.sh

--- install.sh
+++ install.sh
@@ -10,12 +10,15 @@ fi
 create_link() {
   dotfile=$1
   filepath=$2
+
   echo "Linking ${dotfile} -> ${filepath}"
   ln -s $filepath $dotfile
 }
 
 update_link() {
   dotfile=~/.$1
+  filepath=$DOTFILES/$1
+
   if [ ! -e $dotfile ]; then
     create_link $dotfile $filepath
   elif [ -L $dotfile ]; then
